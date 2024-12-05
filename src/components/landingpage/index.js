13e2b8c69614ccbc2710d3f18b2969344b113ff0
nausheen5327 <nausheen.khan@openplaytech.com>
2024-12-05 13:44:39 +0530

    ui fixed

--- src/components/landingpage/index.js
+++ src/components/landingpage/index.js
@@ -1,153 +1,157 @@
 import CssBaseline from '@mui/material/CssBaseline'
 import { useEffect, useState } from 'react'
+import { NoSsr } from '@mui/material'
+import { useDispatch, useSelector } from 'react-redux'
 import BannerSection from './BannerSection'
 import DownloadSection from './DownloadSection'
 import FunFactSection from './FunFactSection'
 import HeroSection from './HeroSection'
 import LinkSection from './link-section/LinkSection'
-
 import { useGetLandingPageData } from '@/hooks/react-query/landing-page/useGetLandingPageData'
 import { setLandingPageData } from '@/redux/slices/storedData'
-import { NoSsr } from '@mui/material'
-import { useDispatch, useSelector } from 'react-redux'
+import { setGlobalSettings } from '@/redux/slices/global'
 import CookiesConsent from '../CookiesConsent'
 import DiscountBanner from './DiscountBanner'
-
-import { setGlobalSettings } from '@/redux/slices/global'
 import AvailableZoneSection from '@/components/landingpage/AvailableZoneSection'
 import AddressList from '../address/addressList'
 
-const LandingPage = (props) => {
-    const { global } = props
-    const [openAddressModal, setOpenAddressModal] = useState(true);
+const LandingPage = ({ global, isAuthenticated }) => {
+    console.log("LandingPage render:", { 
+        hasGlobal: !!global, 
+        isAuthenticated 
+    })
+
     const dispatch = useDispatch()
-    const { landingPageData } = useSelector((state) => state.storedData);
-    const location = useSelector((state) => state.addressData.location);
-    console.log("location", location);
+    const [openAddressModal, setOpenAddressModal] = useState(true)
+    const { landingPageData } = useSelector((state) => state.storedData)
+    const location = useSelector((state) => state.addressData.location)
+    
+    // Get token from localStorage with safety check
+    const [token, setToken] = useState(null)
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const storedToken = localStorage.getItem('token')
+            console.log("Token from localStorage:", !!storedToken)
+            setToken(storedToken)
+        }
+    }, [])
 
-    let token = undefined
-    if (typeof window != 'undefined') {
-        token = localStorage.getItem('token')
-    }
-    const handleModalClose = () => { }
-
+    // Handle landing page data fetching
     const onSuccessHandler = (res) => {
+        console.log("Landing page data fetched successfully")
         dispatch(setLandingPageData(res))
     }
 
     const { data, refetch, isLoading } = useGetLandingPageData(onSuccessHandler)
+
+    // Initial data fetch
     useEffect(() => {
+        console.log("Initiating landing page data fetch")
         refetch()
-    }, [])
-    //   const { data: guestData, refetch: guestRefetch } = useGetGuest();
-    //   useEffect(() => {
-    //     if (!token) {
-    //       guestRefetch();
-    //     }
-    //   }, []);
-    //   useEffect(() => {
-    //     if (typeof window !== 'undefined') {
-    //         localStorage.setItem("guest_id", guestData?.guest_id);
-    //     }
-    //   }, [guestData])
+    }, [token]) // Refetch when token changes
+
+    // Set global settings
     useEffect(() => {
-        dispatch(setGlobalSettings(global))
-    }, [])
+        if (global) {
+            console.log("Setting global settings")
+            dispatch(setGlobalSettings(global))
+        } else {
+            console.warn("Global settings object is missing")
+        }
+    }, [global])
+
+    // Early return if no global settings
+    if (!global) {
+        console.warn("No global settings available, showing loading state")
+        return (
+            <NoSsr>
+                <CssBaseline />
+                <div>Loading...</div>
+            </NoSsr>
+        )
+    }
+
+    const handleModalClose = () => {
+        setOpenAddressModal(false)
+    }
+
+    console.log("Rendering LandingPage content with data:", {
+        hasLandingPageData: !!landingPageData,
+        hasLocation: !!location,
+        isLoading
+    })
 
     return (
         <NoSsr>
             <CssBaseline />
-            {/* {!location && <AddressList openAddressModal={openAddressModal} setOpenAddressModal={setOpenAddressModal} />
-            } */}
+            
+            {/* Address Modal - Only show if authenticated and no location */}
+            {isAuthenticated && !location && (
+                <AddressList 
+                    openAddressModal={openAddressModal} 
+                    setOpenAddressModal={setOpenAddressModal} 
+                />
+            )}
+
             <HeroSection
                 business_name={global?.business_name}
                 banner_section_title={landingPageData?.react_header_title}
-                banner_section_subTitle={
-                    landingPageData?.react_header_sub_title
-                }
-                banner_section_image={
-                    landingPageData?.react_header_image_full_url
-                }
-                banner_section_image_base_url={
-                    landingPageData?.base_urls?.react_header_image_url
-                }
+                banner_section_subTitle={landingPageData?.react_header_sub_title}
+                banner_section_image={landingPageData?.react_header_image_full_url}
+                banner_section_image_base_url={landingPageData?.base_urls?.react_header_image_url}
                 handleModalClose={handleModalClose}
                 isLoading={isLoading}
             />
+
             <FunFactSection
                 global={global}
                 react_feature={landingPageData?.react_services}
                 isLoading={isLoading}
-                fun_base_url={
-                    landingPageData?.base_urls?.react_services_image_url
-                }
+                fun_base_url={landingPageData?.base_urls?.react_services_image_url}
             />
+
             <BannerSection
                 global={global}
                 banner_section_half={landingPageData?.react_promotional_banner}
                 discount_banner={landingPageData?.discount_banner}
                 isLoading={isLoading}
-                promotional_banner_image_url={
-                    landingPageData?.base_urls
-                        ?.react_promotional_banner_image_url
-                }
+                promotional_banner_image_url={landingPageData?.base_urls?.react_promotional_banner_image_url}
             />
-            {landingPageData?.available_zone_status === 1 &&
-                landingPageData?.available_zone_list?.length > 0 && (
-                    <AvailableZoneSection landingPageData={landingPageData} />
-                )}
+
+            {landingPageData?.available_zone_status === 1 && 
+             landingPageData?.available_zone_list?.length > 0 && (
+                <AvailableZoneSection landingPageData={landingPageData} />
+            )}
 
             <LinkSection
-                self_registration_restaurant={
-                    landingPageData?.restaurant_section
-                }
-                self_registration_deliveryMan={
-                    landingPageData?.delivery_section
-                }
-                restaurant_registration_image_url={
-                    landingPageData?.base_urls
-                        ?.react_restaurant_section_image_url
-                }
+                self_registration_restaurant={landingPageData?.restaurant_section}
+                self_registration_deliveryMan={landingPageData?.delivery_section}
+                restaurant_registration_image_url={landingPageData?.base_urls?.react_restaurant_section_image_url}
                 isLoading={isLoading}
-                deliveryman_registration_image_url={
-                    landingPageData?.base_urls?.react_delivery_section_image_url
-                }
+                deliveryman_registration_image_url={landingPageData?.base_urls?.react_delivery_section_image_url}
             />
-            {landingPageData?.download_app_section
-                ?.react_download_apps_banner_image && (
-                    <DiscountBanner
-                        global={global}
-                        discount_banner={
-                            landingPageData?.download_app_section
-                                ?.react_download_apps_banner_image_full_url
-                        }
-                        isLoading={isLoading}
-                        discount_banner_url={
-                            landingPageData?.base_urls
-                                ?.react_download_apps_banner_image_url
-                        }
-                    />
-                )}
 
-            {(landingPageData?.download_app_section
-                ?.react_download_apps_play_store
-                ?.react_download_apps_play_store_status === '1' ||
-                landingPageData?.download_app_section
-                    ?.react_download_apps_app_store
-                    ?.react_download_apps_link_status === '1') && (
-                    <DownloadSection
-                        download_app_data={landingPageData?.download_app_section}
-                        isLoading={isLoading}
-                        global={global}
-                        landing_page_links={landingPageData?.landing_page_links}
-                        download_app_image_urls={
-                            landingPageData?.base_urls
-                                ?.react_download_apps_image_url
-                        }
-                    />
-                )}
+            {landingPageData?.download_app_section?.react_download_apps_banner_image && (
+                <DiscountBanner
+                    global={global}
+                    discount_banner={landingPageData?.download_app_section?.react_download_apps_banner_image_full_url}
+                    isLoading={isLoading}
+                    discount_banner_url={landingPageData?.base_urls?.react_download_apps_banner_image_url}
+                />
+            )}
 
-            <CookiesConsent text={global?.cookies_text} />
+            {(landingPageData?.download_app_section?.react_download_apps_play_store?.react_download_apps_play_store_status === '1' ||
+              landingPageData?.download_app_section?.react_download_apps_app_store?.react_download_apps_link_status === '1') && (
+                <DownloadSection
+                    download_app_data={landingPageData?.download_app_section}
+                    isLoading={isLoading}
+                    global={global}
+                    landing_page_links={landingPageData?.landing_page_links}
+                    download_app_image_urls={landingPageData?.base_urls?.react_download_apps_image_url}
+                />
+            )}
+
+            {/* <CookiesConsent text={global?.cookies_text} /> */}
         </NoSsr>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,194 +1,289 @@
-import LandingPage from '../components/landingpage'
-import React, { useEffect } from 'react'
-import Meta from '../components/Meta'
-import { setGlobalSettings, setIsLoading } from "@/redux/slices/global"
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import Router, { useRouter } from "next/router";
-import { CustomHeader } from "@/api/Headers"
+import { useRouter } from "next/router"
+import { setGlobalSettings, setIsLoading } from "@/redux/slices/global"
 import { setAddressList } from '@/redux/slices/customer'
-import useCancellablePromise from '@/api/cancelRequest'
-import { getCall } from '@/api/MainApi'
 import { setlocation } from '@/redux/slices/addressData'
-import cart, { setCartList } from '@/redux/slices/cart'
+import { setCartList } from '@/redux/slices/cart'
+import { getCall } from '@/api/MainApi'
+import useCancellablePromise from '@/api/cancelRequest'
+import Meta from '../components/Meta'
+import LandingPage from '../components/landingpage'
 import { withAuth } from '@/components/withAuth'
 
-const Home = ({ configData, landingPageData, searchQuery }) => {
-    const router = useRouter();
-    const dispatch = useDispatch();
-    const { cancellablePromise } = useCancellablePromise();
-    const addressList = useSelector((state) => state.user.addressList);
-    console.log(addressList,"address list");
-    let user ;
-    let localLocation ='';
-    useEffect(()=>{
-        if(localStorage.getItem('location'))
-        {
-            localLocation = JSON.parse(localStorage.getItem('location'));
-        }
-        if(localStorage.getItem('user'))
-        {
-            user = JSON.parse(localStorage.getItem('user'));
-        }
-    },[])
-    //here check for address selected
-    // const { deliveryAddress, billingAddress, setDeliveryAddress } =
-    // useContext(AddressContext);
-    const fetchDeliveryAddress = async () => {
-        // setFetchDeliveryAddressLoading(true);
-        dispatch(setIsLoading(true));
-        try {
-            let data = await cancellablePromise(
-              getCall("/clientApis/v1/delivery_address")
-            );
-            dispatch(setIsLoading(false));
+const Home = ({ configData, landingPageData, searchQuery, isAuthenticated }) => {
+  console.log("==== Component Mount ====")
+  console.log("Initial props:", { 
+      hasConfigData: !!configData, 
+      hasLandingPageData: !!landingPageData,
+      isAuthenticated 
+  })
 
-            if(localLocation)
-            {
-                console.log("testing...",localLocation);
-                const findIndex = data.findIndex((item) => item.id === localLocation.id);
-                console.log("testing...",findIndex);
-                if(findIndex!==-1){
-                    dispatch(setlocation(localLocation));
-                    localStorage.setItem('location',JSON.stringify(data[findIndex]));
-                    router.replace('/home')
-                }
-            }
-            // if (deliveryAddress) {
-            //   const findIndex = data.findIndex((item) => item.id === deliveryAddress.id);
-            //   data.unshift(data.splice(findIndex, 1)[0]);
-            // }
-            // setDeliveryAddresses(data);
-            dispatch(setAddressList(data));
-          } catch (err) {
-            dispatch(setIsLoading(false));
-            if (err?.response?.data?.length > 0) {
-              return;
-            }
-            //toast for error in fetching addresses
-          } finally {
-            dispatch(setIsLoading(false));
-            // setFetchDeliveryAddressLoading(false);
-          }
-      };
+  const router = useRouter()
+  const dispatch = useDispatch()
+  const { cancellablePromise } = useCancellablePromise()
+  const addressList = useSelector((state) => state.user.addressList)
+  
+  const [userData, setUserData] = useState(null)
+  const [locationData, setLocationData] = useState(null)
+  const [token, setToken] = useState(null)
 
-
-    dispatch(setGlobalSettings(configData));
-    const getCartItems = async () => {
-        try {
-        //   setLoading(true);
-        dispatch(setIsLoading(true));
-          const url = `/clientApis/v2/cart/${user.localId}`;
-          const res = await getCall(url);
-          dispatch(setIsLoading(false));
-
-          console.log("cart...",res);
-          dispatch(setCartList(res));
-          //add in cart 
-        //   setCartItems(res);
-        //   updatedCartItems.current = res;
-        //   if (setCheckoutCartItems) {
-        //     setCheckoutCartItems(res);
-        //   }
-        } catch (error) {
-            dispatch(setIsLoading(false));
-
-          console.log("Error fetching cart items:", error);
-        //   setLoading(false);
-        } finally {
-            dispatch(setIsLoading(false));
-
-        //   setLoading(false);
-        }
-      };
-
-     
-    useEffect(() => {
-        //use this logic for login
-        // const isLoggedIn = checkIfUserIsLoggedIn(); 
-        // if (!isLoggedIn) {
-        //     router.push('/login'); 
-        //     return;
-        // }
-       //use this logic for login
-      //  console.log("bhaiii token");
+  // First useEffect - initialization
+  useEffect(() => {
+      console.log("==== Initialization Effect ====")
       
-        console.log("bhaiii token",localStorage.getItem('token'));
-        if(localStorage.getItem('token'))
-        {
-         dispatch(setGlobalSettings(configData));
-         fetchDeliveryAddress();
-         getCartItems();
-        }
-         if(!localStorage.getItem('token') && localStorage.getItem('location'))
-         {
-           dispatch(setlocation(JSON.parse(localStorage.getItem('location'))));
-           router.replace('/home')
-         }
+      const currentToken = localStorage.getItem('token')
+      console.log("Token from localStorage:", !!currentToken)
+      setToken(currentToken)
+
+      const storedLocation = localStorage.getItem('location')
+      const storedUser = localStorage.getItem('user')
       
-     
-       
-    }, []);
-    return (
-        <>
-            <Meta
-                title={"ONDC"}
-                ogImage={`https://ondcpreprod.nazarasdk.com/static/media/logo1.ae3b79430a977262a2e9.jpg`}
-            />
-                {configData && (
-                    <LandingPage
-                        global={configData}
-                        landingPageData={landingPageData}
-                    />
-                )}
-        </>
-    );
+      console.log("Stored data present:", {
+          hasStoredLocation: !!storedLocation,
+          hasStoredUser: !!storedUser
+      })
+      
+      if (storedLocation) {
+          const parsedLocation = JSON.parse(storedLocation)
+          setLocationData(parsedLocation)
+      }
+      
+      if (storedUser) {
+          const parsedUser = JSON.parse(storedUser)
+          setUserData(parsedUser)
+      }
+
+      // Set global settings immediately
+      if (configData) {
+          console.log("Setting global settings with configData")
+          dispatch(setGlobalSettings(configData))
+      } else {
+          console.warn("No configData available for global settings")
+      }
+  }, [])
+  const fetchDeliveryAddress = async () => {
+    console.log("Fetching delivery address")
+    dispatch(setIsLoading(true))
+    try {
+        const data = await cancellablePromise(
+            getCall("/clientApis/v1/delivery_address")
+        )
+        console.log("Delivery address data:", data)
+        
+        if (locationData) {
+            const findIndex = data.findIndex((item) => item.id === locationData.id)
+            if (findIndex !== -1) {
+                dispatch(setlocation(locationData))
+                localStorage.setItem('location', JSON.stringify(data[findIndex]))
+            }
+        }
+        
+        dispatch(setAddressList(data))
+    } catch (err) {
+        console.error('Error fetching delivery address:', err)
+    } finally {
+        dispatch(setIsLoading(false))
+    }
 }
 
-// Example function to check if user is logged in
-const checkIfUserIsLoggedIn = () => {
-    // You can check for a token in localStorage or cookies
-    const token = localStorage.getItem('token'); // Example token key
-    // alert(token);
-    return !!token; // Returns true if token exists, false otherwise
+const getCartItems = async () => {
+    console.log("Getting cart items, userData:", userData)
+    if (!userData?.localId) {
+        console.log("No user ID found, checking localStorage")
+        const storedUser = localStorage.getItem('user')
+        if (!storedUser) {
+            console.log("No user found in localStorage")
+            return
+        }
+        const parsedUser = JSON.parse(storedUser)
+        if (!parsedUser.localId) {
+            console.log("No localId in stored user data")
+            return
+        }
+        console.log("Found user ID in localStorage:", parsedUser.localId)
+    }
+    
+    dispatch(setIsLoading(true))
+    try {
+        const userForCart = userData || JSON.parse(localStorage.getItem('user'))
+        const url = `/clientApis/v2/cart/${userForCart.localId}`
+        console.log("Fetching cart from URL:", url)
+        const res = await getCall(url)
+        console.log("Cart response:", res)
+        dispatch(setCartList(res))
+    } catch (error) {
+        console.error("Error fetching cart items:", error)
+    } finally {
+        dispatch(setIsLoading(false))
+    }
 }
-
-export default withAuth(Home,false);
-
-
-
-// pages/index.js
-export async function getServerSideProps(context) {
-    const { query } = context
-  
-    // Your existing data fetching logic
-    let configData = {
-      business_name: "ONDC",
-      // ... other config data
-    }
-  
-    let landingPageData = {
-      banner_section_full: { 
-        banner_section_img_full: "https://source.unsplash.com/random/400x300" 
+  // Token-dependent operations
+  useEffect(() => {
+      console.log("==== Token Effect ====")
+      console.log("Current state:", {
+          hasToken: !!token,
+          hasConfigData: !!configData,
+          hasUserData: !!userData,
+          hasLocationData: !!locationData
+      })
+      
+      if (token) {
+          console.log("Authenticated state - fetching user data")
+          fetchDeliveryAddress()
+          getCartItems()
+      } else if (!token && locationData) {
+          console.log("Unauthenticated with location - redirecting")
+          dispatch(setlocation(locationData))
+          router.replace('/home')
       }
-    }
-  
-    // If there's a search query, fetch search results
-    let searchResults = null
-    if (query.query) {
-      try {
-        // Add your search API call here
-        // searchResults = await fetchSearchResults(query.query)
-      } catch (error) {
-        console.error('Search error:', error)
-      }
-    }
-  
-    return {
-      props: {
-        configData,
-        landingPageData,
-        searchResults,
-        searchQuery: query.query || null
-      }
-    }
+  }, [token, userData, locationData])
+
+  // Render validation
+  useEffect(() => {
+      console.log("==== Render Validation ====")
+      console.log("Render state:", {
+          hasToken: !!token,
+          hasConfigData: !!configData,
+          hasLandingPageData: !!landingPageData,
+          isAuthenticated
+      })
+  }, [token, configData, landingPageData, isAuthenticated])
+
+  if (!configData) {
+      console.warn("Rendering fallback - configData missing")
+      return (
+          <div className="p-4">
+              <Meta title="ONDC" />
+              <div>Loading...</div>
+          </div>
+      )
   }
+
+  console.log("Rendering main component")
+  return (
+      <>
+          <Meta
+              title={"ONDC"}
+              ogImage={`https://ondcpreprod.nazarasdk.com/static/media/logo1.ae3b79430a977262a2e9.jpg`}
+          />
+          <LandingPage
+              global={configData}
+              landingPageData={landingPageData}
+              isAuthenticated={isAuthenticated}
+          />
+      </>
+  )
+}
+
+export default Home
+// pages/index.js
+// export async function getServerSideProps(context) {
+//     const { query } = context
+  
+//     // Your existing data fetching logic
+//     let configData = {
+//       business_name: "ONDC",
+//       // ... other config data
+//     }
+  
+//     let landingPageData = {
+//       banner_section_full: { 
+//         banner_section_img_full: "https://source.unsplash.com/random/400x300" 
+//       }
+//     }
+  
+//     // If there's a search query, fetch search results
+//     let searchResults = null
+//     if (query.query) {
+//       try {
+//         // Add your search API call here
+//         // searchResults = await fetchSearchResults(query.query)
+//       } catch (error) {
+//         console.error('Search error:', error)
+//       }
+//     }
+  
+//     return {
+//       props: {
+//         configData,
+//         landingPageData,
+//         searchResults,
+//         searchQuery: query.query || null
+//       }
+//     }
+//   }
+
+
+export async function getServerSideProps(context) {
+  const { query, req } = context
+  
+  try {
+      // Get the auth token from cookies
+      const token = req.cookies.token || ''
+      
+      // Fetch real config data
+      let configData
+      try {
+          configData = await getCall('/clientApis/v1/config')  // Adjust endpoint as per your API
+      } catch (error) {
+          console.error('Error fetching config:', error)
+          configData = {
+              business_name: "ONDC",
+              // Add other fallback config data
+          }
+      }
+
+      // Fetch landing page data
+      let landingPageData
+      try {
+          landingPageData = await getCall('/clientApis/v1/landing-page')  // Adjust endpoint as per your API
+      } catch (error) {
+          console.error('Error fetching landing page data:', error)
+          landingPageData = {
+              banner_section_full: { 
+                  banner_section_img_full: "https://source.unsplash.com/random/400x300" 
+              }
+          }
+      }
+
+      // Handle search query if present
+      let searchResults = null
+      if (query.query) {
+          try {
+              searchResults = await getCall(`/clientApis/v1/search?q=${query.query}`)  // Adjust endpoint as per your API
+          } catch (error) {
+              console.error('Search error:', error)
+          }
+      }
+
+      return {
+          props: {
+              configData,
+              landingPageData,
+              searchResults,
+              searchQuery: query.query || null,
+              isAuthenticated: !!token  // Pass authentication status to component
+          }
+      }
+  } catch (error) {
+      console.error('getServerSideProps error:', error)
+      // Return default props in case of error
+      return {
+          props: {
+              configData: {
+                  business_name: "ONDC",
+                  // Add other necessary default config
+              },
+              landingPageData: {
+                  banner_section_full: { 
+                      banner_section_img_full: "https://source.unsplash.com/random/400x300" 
+                  }
+              },
+              searchResults: null,
+              searchQuery: null,
+              isAuthenticated: false
+          }
+      }
+  }
+}
