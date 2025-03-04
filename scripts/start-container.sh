8bd1d9a0bccdfadcce702c85346fe78ad36d901c
nausheen5327 <nausheen.khan@openplaytech.com>
2025-03-04 14:50:42 +0530

    bash file

---
+++ scripts/start-container.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+AWS_REGION="ap-south-1"AWS_ACCOUNT_ID="784084668816"
+REPO_NAME="ondc-nazara-staging"
+
+echo "Logging into Amazon ECR..."
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.comIMAGE_URI=$(jq -r '.[0].imageUri' /home/ubuntu/imagedefinitions.json)CONTAINER_NAME=ondc-nazara-frontendecho "Stopping existing container..."docker stop $CONTAINER_NAME || true
+docker rm $CONTAINER_NAME || true
+
+echo "Pulling latest image from ECR..."
+docker pull  https://$IMAGE_URI
+
+echo "Running new container..."
+docker run -d --name $CONTAINER_NAME -p 80:3000 $IMAGE_URI
